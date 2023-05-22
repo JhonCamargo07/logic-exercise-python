f6137e108a12072d21de98ce2354294ac4ed9a6e
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2023-05-22 07:48:31 -0500

    Mas ejercicios

--- nums/dia_habil.py
+++ nums/dia_habil.py
@@ -1,52 +1,20 @@
 """
 Un programa que ingresando una fecha, me muestre cuando se se cumplen 15 dias habililes a partir del dia proporcionado
+sin tener en cuenta los festivos
 """
 from datetime import datetime
 from datetime import timedelta
 
 
-def dia(fecha):
-    print('Fecha actual', fecha, fecha.strftime('%A'))
-
+def get_dia_habil(fecha):
+    print('Fecha actual', fecha.date())
+    fecha += timedelta(days=21)
     curren_day = fecha.strftime('%A')
-    count = 0
-    if curren_day == 'Sunday' or curren_day == 'Saturday':
-        fecha += timedelta(days=21)
-        print('festivo',  fecha.strftime('%A'))
-        if fecha.strftime('%A') == 'Sunday':
-            fecha += timedelta(days=1)
-        elif fecha.strftime('%A') == 'Saturday':
-            fecha += timedelta(days=2)
-        return fecha
-    else:
-        return fecha + timedelta(days=21)
-
-    # test = fecha.date()
-    # fecha2 = fecha.date()
-    # for day in range(count):
-    #     test = test + timedelta(days=1)
-    #     test2 = test.strftime('%A')
-    #     if test2 != 'Sunday' and test2 != 'Saturday':
-    #         fecha2 = fecha + timedelta(days=1)
-    #
-    #     print(fecha2)
-
-    # print(fecha2.date(),  test, test - fecha2.date())
-    # return test
+    if curren_day == 'Sunday':
+        fecha -= timedelta(days=2)
+    elif curren_day == 'Saturday':
+        fecha -= timedelta(days=1)
+    return fecha.date()
 
 
-# Obtener la fecha actual
-# fecha_actual = datetime.now().date()
-
-# Obtener el día de la semana
-# dia_semana = datetime.now()
-# dia_semana = dia_semana - timedelta(days=1)
-# dia_semana = dia_semana.strftime('%A')
-# Convertir la fecha y el día a str
-# fecha_actual_str = fecha_actual.strftime('%Y-%m-%d')
-
-# print(f"Fecha actual: {fecha_actual_str}")
-# print(f"Día de la semana: {dia_semana}")
-
-
-print('Fecha proxima', dia(datetime.now() - timedelta(days=1)))
+print('Fecha proxima', get_dia_habil(datetime.now() - timedelta(days=0)))
