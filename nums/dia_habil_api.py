9b849289badc7f345474be8587b28da01d83d7ba
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2023-06-02 08:13:44 -0500

    Mas ejercicios

--- nums/dia_habil_api.py
+++ nums/dia_habil_api.py
@@ -1,6 +1,8 @@
 import requests
+from datetime import datetime, timedelta
 
-def obtener_festivos_colombia(year):
+
+def get_festivos_COL(year):
     url = f"https://date.nager.at/api/v3/PublicHolidays/{year}/CO"
     response = requests.get(url)
     data = response.json()
@@ -12,22 +14,20 @@ def obtener_festivos_colombia(year):
     print(festivos)
     return festivos
 
+
 def calcular_siguientes_dias_habiles(fecha, cantidad):
-    festivos = obtener_festivos_colombia(fecha.year)
+    festivos = get_festivos_COL(fecha.year)
     dias_habiles = []
     dia_actual = fecha
     while len(dias_habiles) < cantidad:
         dia_actual += timedelta(days=1)
         if dia_actual.weekday() < 5 and str(dia_actual.date()) not in festivos:
-            dias_habiles.append(dia_actual)
+            dias_habiles.append(dia_actual.date())
     return dias_habiles
 
-# Ejemplo de uso
-from datetime import datetime, timedelta
 
 fecha_inicial = datetime(2023, 6, 1)  # Fecha inicial proporcionada
 cantidad_dias = 15  # Cantidad de días hábiles a calcular
 
 dias_habiles = calcular_siguientes_dias_habiles(fecha_inicial, cantidad_dias)
-for dia in dias_habiles:
-    print(dia.strftime("%Y-%m-%d"))
+print(f'Ultimo día habil: {dias_habiles[-1]}')
